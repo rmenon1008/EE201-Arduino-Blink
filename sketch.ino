b088e5fb3edc1458e23c54848b57285963b56517
rmenon1008 <rmenon1008@gmail.com>
2022-10-19 12:48:45 -0700

    first commit

---
+++ sketch.ino
@@ -0,0 +1,17 @@
+// https://wokwi.com/projects/345976721310220882
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  float period = map(analogRead(A0), 0, 1023, 5000, 200);
+
+  digitalWrite(13, HIGH);
+  delay(50);
+  digitalWrite(13, LOW);
+  
+  delay(period);
+}
